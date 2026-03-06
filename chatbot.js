8b1beeaeb60676131656e5f0fbd025f8211d2986
Axel-lol29 <axalberto250@gmail.com>
2026-03-06 14:36:41 -0600

    Cambio ggg

--- chatbot.js
+++ chatbot.js
@@ -48,7 +48,7 @@ async function sendMessage(){
 
   try{
 
-    const res = await fetch("http://localhost:5678/webhook/chatbot",{
+    const res = await fetch("https://laquita-noncontrolled-deneen.ngrok-free.dev/webhook/chatbot",{
       method:"POST",
       headers:{ "Content-Type":"application/json"},
       body:JSON.stringify({message:text})
